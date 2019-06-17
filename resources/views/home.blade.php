046ea240b6ed354cc825087ce41c2b13d9616fa4
root <root@localhost.localdomain>
2019-06-17 21:52:24 +0800

    修改：     .editorconfig 	删除：     .env.example 	修改：     .gitattributes 	修改：     .github/ISSUE_TEMPLATE/bug_report.md 	修改：     .github/ISSUE_TEMPLATE/feature_request.md 	修改：     .gitignore 	修改：     CONTRIBUTING.md 	修改：     LICENSE 	修改：     app/Admin/Controllers/AuthController.php 	修改：     app/Admin/Controllers/ExampleController.php 	修改：     app/Admin/Controllers/HomeController.php 	修改：     app/Admin/bootstrap.php 	修改：     app/Admin/routes.php 	修改：     app/Base.php 	修改：     app/Console/Kernel.php 	修改：     app/Exceptions/Handler.php 	修改：     app/GD/Draw.php 	修改：     app/Http/Controllers/Auth/ForgotPasswordController.php 	修改：     app/Http/Controllers/Auth/LoginController.php 	修改：     app/Http/Controllers/Auth/RegisterController.php 	修改：     app/Http/Controllers/Auth/ResetPasswordController.php 	修改：     app/Http/Controllers/Auth/VerificationController.php 	修改：     app/Http/Controllers/Controller.php 	修改：     app/Http/Controllers/DrawController.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,13 +39,6 @@
                     <button id="vie" class="btn btn-primary">预览</button>
                 </div>
                 <div class="card-body" style="height: 80%;overflow-y: scroll;">
-					<div class='main'>
-						<input type='file' class='filebutton' style='display:none' onchange='fileSelected()'  /> <br>
-						<button class="upload" onclick='openFileDialog()' > 选择文件上传 </button>
-						<div class="img">
-						</div>
-				   </div>
-
                     <img id="img" style="width:100%;"/>
                     <div id="debug"></div>
                 </div>
@@ -117,74 +110,5 @@
         div.appendChild(document.createTextNode(str));
         return div.innerHTML;
     }
-</script>
-<script>
- //点击普通按钮,打开文件选择框
-	    function openFileDialog()
-	    {
-	    	$(".filebutton").click();
-	    }
-	    //选择一个文件时onchange时间被触发
-	    function fileSelected()
-	    {
-	    	var fbutton = $(".filebutton")[0];//dom元素
-	    	//读取文件
-	    	var reader = new FileReader();
-
-
-	    	var file = fbutton.files[0];
-	    	reader.readAsDataURL(file);
-	    	startFileUpload(file);	    	
-	    }
-	    //开始上传
-	    function startFileUpload(file)
-	    {
-	    	var uploadURL = "{{ Route('uploadimg') }}";
-	    	
-	    	var formData = new FormData();
-	    	formData.append("file" , file);
-			formData.append("_token" , '{{ csrf_token() }}');
-	    	var request = new XMLHttpRequest();
-	    	request.upload.addEventListener("progress" , window.evt_upload_progress , false);
-		    request.addEventListener("load", function (evt)
-			{
-				if(evt.loaded == 0)
-				{
-					console.log ("上传失败!");
-				}
-				else
-				{
-					console.log ("上传完成!");
-					var response = JSON.parse(evt.target.responseText);
-					var reader = new FileReader();
-					var f1='<table border="1"><tr><th>userimg/'
-					+response.md5+'</th><th>'
-					+'<img src="'+reader.readAsDataURL(file)+'"/></th></tr>';
-					f1+='</table>';
-					document.getElementById('debug').innerHTML=f1;
-				}			
-			}, false);
-		    request.addEventListener("error", window.evt_upload_failed, false);
-		    request.addEventListener("abort", window.evt_upload_cancel, false);			
-			request.open("POST", uploadURL ); // 设置服务URL
-		    request.send(formData);  // 发送表单数据
-	    }
-	    window.evt_upload_progress = function(evt)
-	    {
-	    	if(evt.lengthComputable)
-	    	{
-	    		
-	    		var progress = Math.round(evt.loaded * 100 / evt.total);
-	    		console.log("上传进度" + progress);
-	    	}
-	    };		 
-		window.evt_upload_failed = function (evt) 
-		{			
-			console.log  ("上传出错"); 
-		};
-		window.evt_upload_cancel = function (evt) 
-		{
-			console.log( "上传中止!");	
-		};
 </script>
 @endsection
